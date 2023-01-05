d14634669c874ce9bf3fcd4c07035cbc3be2ec8e
somebodysmokein <venky88r@gmail.com>
2023-01-06 01:11:44 +0530

    Updated with the on-prem run
    Conftest file has been updated to include on-prem run

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import urllib
 
-
 import pytest
 from browserstack.local import Local
 import os, json
@@ -22,17 +21,20 @@ bs_local = None
 timenow = None
 
 BROWSERSTACK_USERNAME = os.environ['BROWSERSTACK_USERNAME'] if 'BROWSERSTACK_USERNAME' in os.environ else CONFIG["user"]
-BROWSERSTACK_ACCESS_KEY = os.environ['BROWSERSTACK_ACCESS_KEY'] if 'BROWSERSTACK_ACCESS_KEY' in os.environ else CONFIG["key"]
+BROWSERSTACK_ACCESS_KEY = os.environ['BROWSERSTACK_ACCESS_KEY'] if 'BROWSERSTACK_ACCESS_KEY' in os.environ else CONFIG[
+    "key"]
+
 
 def start_local():
     """Code to start browserstack local before start of test."""
     global bs_local
     global timenow
-    timenow=str(time.time()*1000)
+    timenow = str(time.time() * 1000)
     print(timenow)
     bs_local = Local()
-    bs_local_args = { "key": BROWSERSTACK_ACCESS_KEY or "access_key", "localIdentifier": timenow }
+    bs_local_args = {"key": BROWSERSTACK_ACCESS_KEY or "access_key", "localIdentifier": timenow}
     bs_local.start(**bs_local_args)
+
 
 def stop_local():
     """Code to stop browserstack local after end of test."""
@@ -42,36 +44,53 @@ def stop_local():
 
 
 if os.environ['REMOTE'] == "true":
-#if "true" == "true":
-  @pytest.fixture(scope='session')
-  def session_capabilities(playwright: Playwright):
-    global timenow
-    test_name = os.environ.get('PYTEST_CURRENT_TEST').split(' ')[0].split('::')[1]
-    capabilities = merge(CONFIG['environments'][TASK_ID],CONFIG["capabilities"])
-    capabilities['browserstack.username'] = BROWSERSTACK_USERNAME
-    capabilities['browserstack.accessKey'] = BROWSERSTACK_ACCESS_KEY
-    capabilities['source'] = 'pytest:sample-main:v1.0'
-    capabilities['sessionName'] = test_name
-    print(CONFIG['base_url'])
-    if "local" in capabilities and capabilities['local']:
-        start_local()
-        capabilities['browserstack.local'] = "true"
-        capabilities['browserstack.localIdentifier'] = timenow
-    print("capabilities => "+json.dumps(capabilities))
-    stringifiedCaps = urllib.parse.quote(json.dumps(capabilities))
-    caps = 'wss://cdp.browserstack.com/playwright?caps=' + stringifiedCaps
-    browser = playwright.chromium.connect(str(caps))
-    context = browser.new_context()
-    page = context.new_page()
-    yield page
-    context.close()
-    browser.close()
+    @pytest.fixture(scope='session')
+    def session_capabilities(playwright: Playwright):
+        global timenow
+        test_name = os.environ.get('PYTEST_CURRENT_TEST').split(' ')[0].split('::')[1]
+        capabilities = merge(CONFIG['environments'][TASK_ID], CONFIG["capabilities"])
+        capabilities['browserstack.username'] = BROWSERSTACK_USERNAME
+        capabilities['browserstack.accessKey'] = BROWSERSTACK_ACCESS_KEY
+        capabilities['source'] = 'pytest:sample-main:v1.0'
+        capabilities['sessionName'] = test_name
+        print(CONFIG['base_url'])
+        if "local" in capabilities and capabilities['local']:
+            start_local()
+            capabilities['browserstack.local'] = "true"
+            capabilities['browserstack.localIdentifier'] = timenow
+        print("capabilities => " + json.dumps(capabilities))
+        stringifiedCaps = urllib.parse.quote(json.dumps(capabilities))
+        caps = 'wss://cdp.browserstack.com/playwright?caps=' + stringifiedCaps
+        browser = playwright.chromium.connect(str(caps))
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        context.close()
+        browser.close()
+else:
+    @pytest.fixture(scope='session')
+    def session_capabilities(playwright: Playwright):
+        capabilities = CONFIG['environments'][TASK_ID]
+        if "browser" in capabilities and capabilities['browser'] == 'chrome':
+            browser = playwright.chromium.launch(channel='chrome', headless=False)
+        if "browser" in capabilities and capabilities['browser'] == 'edge':
+            browser = playwright.chromium.launch(channel='msedge', headless=False)
+        elif "browser" in capabilities and capabilities['browser'] == 'firefox':
+            browser = playwright.firefox.launch(headless=False)
+        else:
+            browser = playwright.webkit.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        context.close()
+        browser.close()
+
 
 @pytest.fixture(scope='session')
 def base_url():
-  print(CONFIG['base_url'])
-  return CONFIG['base_url']
+    print(CONFIG['base_url'])
+    return CONFIG['base_url']
+
 
 def pytest_sessionfinish(session, exitstatus):
-  stop_local()
-
+    stop_local()
